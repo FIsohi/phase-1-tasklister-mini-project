e3e1d9395b9aeadbdad1b0759377e349ab1c1ad3
Faith Isohi <faith.isohi@student.moringaschool.com>
2024-04-15 18:56:09 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //grabs all the necessary DOM elements.
+
+  //set the newTaskForm to a variable.
+  const newTaskForm = document.querySelector('form');
+
+  //attach a submit event listener to newTaskForm.
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //create a variable called newToDo that holds the value of the targeted input.
+    const newToDo = e.target.querySelector('#new-task-description').value
+
+    //pass in the newToDo variable as an argument to buildToDo. 
+    buildToDo(newToDo);
+
+    //after buildToDo function runs, reset the newTaskForm so the input is empty
+    newTaskForm.reset();
+  })
 });
-const newTaskDescription = document.getElementById("new-task-description");
-const theList = document.getElementById("tasks");
 
-const createNewTask = event => { 
-  event.preventDefault(); 
-  const newTask = document.createElement('li');
+  //when user submits, we pass in the targeted input into buildToDo function
+  function buildToDo(newToDo){
+    //first, the buildToDo function creates an li element and a button element. 
+    const taskLi = document.createElement('li');
+    const deleteButton = document.createElement('button');
 
-  newTask.innerText = newTaskDescription.value;
-  createButton(newTask);
-  theList.appendChild(newTask);
+    //next, we set the textContent of the btn element to 'x'.
+    deleteButton.textContent = 'x';
 
-  event.target.reset();
-};
+    //after, we set the textContent of the li element to the todo parameter.
+    //remember, when we submit, we are passing in targeted input into the parameters. 
+    taskLi.textContent = newToDo;
 
-const createButton = task => {
-   const btn = document.createElement('button');
-    btn.innerText = "X";
+    //next, we append the new btn to the li.
+    taskLi.appendChild(deleteButton);
 
-    task.appendChild(btn);
-    btn.addEventListener('click',  (e) => deleteTask(e));
-}
+    //then, we add the li element to the ul element with id of tasks
+    document.querySelector('#tasks').appendChild(taskLi);
+  
+  
+    //finally, we attach a click event listener to the btn which invokes the handleDelete function
+    deleteButton.addEventListener('click', handleDelete);
+  }
 
-const deleteTask = (e) => {
-  const selectedTask = e.target.parentElement;
-  theList.removeChild(selectedTask);
-}
-
-document.querySelector('#create-task-form').addEventListener("submit"
-, (e) => createNewTask(e))
+  //when the button element with the textContent of 'x' is clicked, this function will run.
+  function handleDelete(e){
+    //this function targets the parent node of the button and removes it.
+    //in this case, the parentNode of the button element is the li element, so the li is removed from the list.
+    e.target.parentNode.remove();
+  }
